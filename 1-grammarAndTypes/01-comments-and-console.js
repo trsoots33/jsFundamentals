2eba191211f29985aae46b9e8663c89eb20ed29d
Ryder <Ryder@News-MacBook-Pro.local>
2021-08-18 19:16:42 -0400

    First commit

---
+++ 1-grammarAndTypes/01-comments-and-console.js
@@ -0,0 +1,38 @@
+/*  
+COMMENTS
+
+1-grammarAndTypes
+01-comments-console.js
+*/
+
+//vaule of a comment:
+//communitcates to anyone reading our code
+
+//types of comments: 
+//1) Single line comment
+
+/* 
+2) multi line comment 
+*/
+
+
+//console.log 
+//outputs some message to the console
+console.log("this is a console.log message");
+console.log("you can separate parts of a console message with commas","just like this");
+console.log(13, 15, 17, 18);
+
+//console log and debeugging
+//we can debug with console.log messages in 2 big ways
+//1) we can check the status of some javascript 'thing'
+//2) we can check a process we have built with repeated console.log messages
+
+//example one:
+var sampleName ="Ryder"
+console.log(sampleName)
+
+//example two:
+var sampleNumber =1;
+sampleNumber++;
+sampleNumber++;
+console.log(sampleNumber);
